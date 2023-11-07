500610e71f4ee0adb5e52c31b02e22db43a94cce
Xu Che <xuche1998@gmail.com>
2023-11-07 18:12:58 +0800

    Add buffer to render function for better performance and fix template file extensions

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -23,12 +23,12 @@ func newTemplateCache() (map[string]*template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		ts, err := template.ParseFiles("./ui/html/base.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
 
-		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl.html")
 		if err != nil {
 			return nil, err
 		}
