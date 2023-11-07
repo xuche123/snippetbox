4ac8b1d655e75a29a844a81813ae9f4dd9946d80
Xu Che <xuche1998@gmail.com>
2023-11-07 18:06:37 +0800

    Refactor template caching in templates.go

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -3,16 +3,17 @@ package main
 import (
 	"html/template"
 	"path/filepath"
+
 	"snippetbox.xuche.net/internal/models"
 )
 
 type templateData struct {
-	Snippet *models.Snippet
+	Snippet  *models.Snippet
 	Snippets []*models.Snippet
 }
 
-func newTemplateCache() (map[string] *template.Template, error) {
-	cache := map[string] *template.Template{}
+func newTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
 
 	pages, err := filepath.Glob("./ui/html/pages/*.tmpl.html")
 	if err != nil {
@@ -22,13 +23,17 @@ func newTemplateCache() (map[string] *template.Template, error) {
 	for _, page := range pages {
 		name := filepath.Base(page)
 
-		files := []string{
-			"./ui/html/base.tmpl.html",
-			"./ui/html/partials/nav.tmpl.html",
-			page,
+		ts, err := template.ParseFiles("./ui/html/base.tmpl")
+		if err != nil {
+			return nil, err
 		}
 
-		ts, err := template.ParseFiles(files...)
+		ts, err = ts.ParseGlob("./ui/html/partials/*.tmpl")
+		if err != nil {
+			return nil, err
+		}
+
+		ts, err = ts.ParseFiles(page)
 		if err != nil {
 			return nil, err
 		}
