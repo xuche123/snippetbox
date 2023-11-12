730f764b5d2a71faad4439e522414577a4c6c87e
Xu Che <xuche1998@gmail.com>
2023-11-12 12:45:25 +0800

    Add create snippet form and navigation link

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -49,7 +49,8 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 }
 
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Display the form for creating a new snippet..."))
+	data := app.newTemplateData(r)
+	app.render(w, http.StatusOK, "create.tmpl.html", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
