7dc5ecae0c5a1752faa391c0a786aed797b69ba2
Xu Che <xuche1998@gmail.com>
2023-11-12 12:45:37 +0800

    Add form validation to snippet creation

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -10,8 +10,9 @@ import (
 
 type templateData struct {
 	CurrentYear int
-	Snippet  *models.Snippet
-	Snippets []*models.Snippet
+	Snippet     *models.Snippet
+	Snippets    []*models.Snippet
+	Form        any
 }
 
 func humanDate(t time.Time) string {
